65185b04a661196df3a9470837f218098fdaa1f2
Kyle Diggs <Contact@KyleDiggs.com>
2017-05-08 17:45:48 -0700

    Did A TON of work and got the first page set up. Probably should have commited more often but oh well!
    Foundation is good. Latest change was to the sidebar to make it fixed.
    Currently is bug free as far as I can tell!
    
    Signed-off-by: Kyle Diggs <Contact@KyleDiggs.com>

--- JS/base.js
+++ JS/base.js
@@ -2,7 +2,9 @@ $(document).ready( function (){
     $(".sidebar > .button-con > .button").on("click", function () {
         if ($(".sidebar").hasClass("extended")) {
             if ($(this).hasClass("highlight")) {
-                $(".sidebar").removeClass("extended");    
+                $(".sidebar").removeClass("extended");   
+                $(".sidebar > .button-con").removeClass("extended");
+                $(".page").removeClass("extended");   
                 $(this).removeClass("highlight");   
             } else {
                 $(".sidebar > .button-con > .button").removeClass("highlight");
@@ -10,6 +12,8 @@ $(document).ready( function (){
             }
         } else {
             $(".sidebar").addClass("extended");    
+            $(".sidebar > .button-con").addClass("extended");   
+            $(".page").addClass("extended");   
             $(this).addClass("highlight");
         }
     });
